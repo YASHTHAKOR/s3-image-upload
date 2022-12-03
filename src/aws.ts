c80a2985a42ce769c4578b066ed3e6cfb4f177ab
yashthakor4595 <yashthakor4595@gmail.com>
2022-12-04 01:09:02 +0530

    set up s3 upload

---
+++ src/aws.ts
@@ -0,0 +1,32 @@
+import AWS from 'aws-sdk'
+import { lookup } from 'mime-types'
+
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+    path: '.env'
+});
+
+export type SignedUrlParams = {
+    uploadUrl: string
+}
+
+const S3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    signatureVersion: 'v4',
+})
+
+export const getSignedUrl = (key: string, path: string): SignedUrlParams => {
+    const fileType: string = lookup(key) as string
+    const params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: path,
+        Expires: 2 * 60,
+        ContentType: fileType,
+    }
+    const uploadUrl: string = S3.getSignedUrl('putObject', params)
+    return {
+        uploadUrl,
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import {getSignedUrl} from './aws';
+
+const index: Application = express()
+const port = 3000;
+
+
+index.use(cors())
+index.use(express.json());
+index.use(express.urlencoded({ extended: true }));
+
+index.post('/image/s3/updload', async (req: Request, res: Response) => {
+    try {
+        const url = await getSignedUrl(req.body.key, req.body.path);
+        res.send(url);
+    } catch (Err) {
+        res.status(500).send(Err);
+    }
+})
+
+index.get('/health', async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({message: `Wallet service up and running`})
+})
+
+
+try {
+    index.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`)
+    })
+} catch (error: any) {
+    console.log(`Error occurred: ${error.message}`)
+}
